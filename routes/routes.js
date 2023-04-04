8f099090c6972568a1fafce47aa656ba0002e092
devhyacinthe <yacinthedalmeida2002@gmail.com>
2023-04-04 16:40:31 +0000

    bug

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const express = require('express')
 const {signin, signup, main} = require('../controllers/ctrl')
 const router = Router()
 
@@ -8,6 +9,15 @@ router.post('/signup', signup)
 router.post('/signin', signin)
 
 router.use(passport.authenticate('jwt', {session: false}))
-router.get('/', main)
+router.get('/main', main)
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+router.get('/', (req, res) => {
+    res.send('hello')
+})
 
 module.exports = router
